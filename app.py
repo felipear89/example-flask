c4eac9b1448c1ac06230ebc1c1728a2d639f72a2
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-11-20 20:22:45 -0200

    add SQLAlchemy and migrations

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import os
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
 app.config.from_object(os.environ['APP_SETTINGS'])
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Todo
 
 
 @app.route('/')
@@ -14,6 +20,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
